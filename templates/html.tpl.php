dc72a8d0fb52ae0f498eaafb1471acf7d9a2cbe9
Fontaine Cook <fontaine.cook@pearance.com>
2012-09-06 06:07:25 -0400

    Add javascript to provide an x-close button for messages

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -2,10 +2,33 @@
 <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf->version . $rdf->namespaces; ?>>
 <head<?php print $rdf->profile; ?>>
   <?php print $head; ?>
-  <title><?php print $head_title; ?></title>  
+  <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
   <?php print $scripts; ?>
   <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+  <script>
+    Drupal.behaviors.prostarter = {
+      attach: function(context, settings) {
+        (function ($) {
+          /* =============================================================================
+            Add 'x' close button and handler to status messages.
+            ========================================================================== */
+          $.fn.closeButtonMessages = function() {
+            $('.messages').each(function() {
+              if ($(this).find('a.close').length < 1) {
+                $(this).prepend('<a class="close" href="#" title="' + Drupal.t('Close') + '">x</a>');
+              }
+            });
+            $('.messages a.close').click(function(e) {
+              e.preventDefault();
+              $(this).parent().fadeOut('slow');
+            });
+          };
+          $().closeButtonMessages();
+        })(jQuery);
+      }
+    }
+  </script>
 </head>
 <body<?php print $attributes;?>>
   <div id="skip-link">
