419653ce11a7e144ebb09b643713f97748fb8d47
Fontaine Cook <fontaine.cook@pearance.com>
2012-11-09 05:10:31 -0500

    Add config.rb, scss files, & styled user menu

---
+++ config.rb
@@ -0,0 +1,19 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "css/scss"
+images_dir = "gfx"
+javascripts_dir = "js"
+relative_assets = true
+line_comments = true
+output_style = :expanded
+# output_style = :compressed
+
+# Additional Options
+# output_style = :expanded or :nested or :compact or :compressed
+# preferred_syntax = :sass
+#
+#
+sass_options = {:debug_info => true}
