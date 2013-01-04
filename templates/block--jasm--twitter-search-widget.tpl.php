9fbfec411b69e3fc082719dd11a16897ba1d5c01
Fontaine Cook <fontaine.cook@pearance.com>
2013-01-04 13:54:52 -0500

    Create jasm-twitter-search-widget template

--- templates/block--jasm--twitter-search-widget.tpl.php
+++ templates/block--jasm--twitter-search-widget.tpl.php
@@ -2,11 +2,9 @@
 <<?php print $tag; ?><?php print $attributes; ?>>
   <div class="block-inner clearfix">
     <?php print render($title_prefix); ?>
-    <?php if ($block->subject): ?>
-      <h2<?php print $title_attributes; ?>><?php print $block->subject; ?></h2>
-    <?php endif; ?>
+    <h2<?php print $title_attributes; ?>>Trending</h2>
     <?php print render($title_suffix); ?>
-    
+
     <div<?php print $content_attributes; ?>>
       <?php print $content ?>
     </div>
