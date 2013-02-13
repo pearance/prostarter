763f459b92ab1f9604e9b4c8c9bfaaae2ea6aade
Fontaine Cook <fontaine.cook@pearance.com>
2013-02-13 06:37:34 -0500

    Tweak class placement in markup

--- templates/user-login.tpl.php
+++ templates/user-login.tpl.php
@@ -8,14 +8,14 @@
 */
 ?>
 
-<a href="/">
+<a class="brand" href="/">
 	<img src="<?php print base_path() . path_to_theme() ?>/gfx/brand.png" alt="">
 </a>
 <div id="user-login-content">
 	<?php print drupal_render($form['name']); ?>
 	<?php print drupal_render($form['pass']); ?>
 	<?php print drupal_render($form['actions']); ?>
-	<a href="password">forgot my password</a>
+	<a class="forgot-login-link" href="password">forgot my password</a>
 </div>
 
 <?php print drupal_render($form['form_build_id']);?>
